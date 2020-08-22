ab90c3b5ca997e18e3b816d4be27b15c27025ab7
umetaniyoshinori <chaniigizumo@gmail.com>
2020-08-22 15:11:27 +0900

    send_message

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "messages#index"
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
+  resources :users, only: [:edit, :update, :edit]
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
